f3ce97ccbda919a6cdd1118e81c13eb72762c29c
David Hileman <dhileman823@gmail.com>
2021-09-19 20:45:30 -0400

    settings widget work

--- public/js/settings.js
+++ public/js/settings.js
@@ -6,12 +6,17 @@ var SettingsWidgetFactory = {
             "domNode":domNode, 
             "props":{},
             "placeAt":null, 
-            "render":null
+            "render":null,
+            "onSignIn":null
         };
 
         settingsWidget.props = {
             "settingsSectionSignInDisplay": "block",
             "settingsSectionContentDisplay": "none"
+        };
+
+        settingsWidget.onSignIn = function(){
+            console.log("in onSignIn");
         };
 
         settingsWidget.render = function(){
@@ -20,16 +25,30 @@ var SettingsWidgetFactory = {
                 this.props.settingsSectionContentDisplay = "block";
             }
             var templateString = 
-            `<div style="margin:5px">
+            `<div class="settings-widget" style="margin:5px">
                 <div class="section-sign-in" style="display:${this.props.settingsSectionSignInDisplay}">
-                    <button class="btn btn-primary">Sign In</button
+                    <button class="btn btn-primary" attach="onSignIn">Sign In</button
                 </div>
                 <div id="settingsSectionContent" style="display:${this.props.settingsSectionContentDisplay}">
                     This is the main content
                 </div>
             </div>`;
             this.domNode.innerHTML = templateString;
+            this.attachEvents();
         };
+
+        settingsWidget.attachEvents = function(){
+            var buttons = document.querySelectorAll(".settings-widget button");
+            for(var i=0;i<buttons.length;i++){
+                var button = buttons[i];
+                var attach = button.getAttribute("attach");
+                if(attach){
+                    console.log(this);
+                    button.addEventListener("click", this[attach]);
+                }
+            }
+        };
+
         settingsWidget.placeAt = function(element){
             element.appendChild(this.domNode);
             this.render();
