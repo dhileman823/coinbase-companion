78509879c163e8f7228fffdb42651da46bbf2b29
David Hileman <dhileman823@gmail.com>
2022-05-27 15:03:37 -0400

    added transaction history component

--- src/components/pages/OrderPage.js
+++ src/components/pages/OrderPage.js
@@ -1,7 +1,35 @@
-function OrderPage(){
-    return (
-        <div className="page">Order page</div>
-    );
+import { useEffect, useState } from "react";
+import TransactionHistory from "./subcomponents/TransactionHistory";
+
+function OrderPage() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const orderList = [
+      {
+        id: 1,
+        amount: 30,
+        asset: "BTC-USD",
+        created: new Date().toDateString(),
+        type: "order",
+      },
+      {
+        id: 2,
+        amount: 30,
+        asset: "ETH-USD",
+        created: new Date().toDateString(),
+        type: "order",
+      },
+    ];
+    setOrders(orderList);
+  }, []);
+
+  return (
+    <div className="page">
+      Transaction History
+      <TransactionHistory orders={orders} />
+    </div>
+  );
 }
 
 export default OrderPage;
---
+++ src/components/pages/subcomponents/TransactionHistory.js
@@ -0,0 +1,43 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function TransactionHistory(props) {
+  const { orders } = props;
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Amount</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Type</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders.map((order) => (
+            <TableRow
+              key={order.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {order.asset}
+              </TableCell>
+              <TableCell align="right">{order.amount}</TableCell>
+              <TableCell align="right">{order.created}</TableCell>
+              <TableCell align="right">{order.type}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default TransactionHistory;
